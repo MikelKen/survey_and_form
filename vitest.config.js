d3ffef0337c2e31f17eea37719cd4de3d775360f
Mikelken <mayki6866@gmail.com>
2026-07-23 10:07:50 -0400

    script vitest

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    timeout: 10000, // en milisegundos (10s),
+    globals: true, // Habilita describe, it, expect globales
+    timeout: 10000,
+    // Le dice a Vitest que SOLO busque pruebas en tu carpeta principal test/
+    include: ["test/**/*.test.js"],
+    // Opcionalmente ignora cualquier otra carpeta de tests
+    exclude: ["**/node_modules/**", "**/tigo/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
